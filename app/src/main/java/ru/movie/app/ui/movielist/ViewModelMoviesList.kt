ff5db1d84a96df14e46ba408308ea203ea311606
Evgenii Shevchenko <eshevchenko@chulakov.com>
2020-12-03 22:11:34 +0300

    MA-3: added other movies to the list

--- app/src/main/java/ru/movie/app/ui/movielist/ViewModelMoviesList.kt
+++ app/src/main/java/ru/movie/app/ui/movielist/ViewModelMoviesList.kt
@@ -8,21 +8,18 @@ import ru.movie.app.ui.model.Movie
 
 class ViewModelMoviesList : ViewModel() {
     private val moviesStore = mutableListOf<Movie>().apply {
-        for (i in 0..5) {
-            this.add(
-                Movie(
-                    0,
-                    "Avengers: End Game",
-                    R.drawable.movie,
-                    "13+",
-                    "Action, Adventure, Fantasy",
-                    4,
-                    0,
-                    true,
-                    137
-                )
-            )
-        }
+        this.add(
+            Movie(0, "Avengers: End Game", R.drawable.movie_avengers, "13+", "Action, Adventure, Fantasy", 4, 0, false, 137)
+        )
+        this.add(
+            Movie(1, "Tenet", R.drawable.movie_tenet, "16+", "Action, Sci-Fi, Thriller", 5, 0, true, 98)
+        )
+        this.add(
+            Movie(2, "Black Widow", R.drawable.movie_black_widow, "13+", "Action, Adventure, Sci-Fi", 4, 0, false, 38)
+        )
+        this.add(
+            Movie(3, "Wonder Woman 1984", R.drawable.movie_wonder_woman, "13+", "Action, Adventure, Fantasy", 5, 0, false, 74)
+        )
     }
 
     private val _moviesLiveData = MutableLiveData<List<Movie>>()
