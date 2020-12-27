d078f2430b70a2adcb799635b05da2ed9402c43f
Evgenii Shevchenko <eshevchenko@chulakov.com>
2020-12-27 23:44:52 +0300

    MA-5: refactoring

--- app/src/main/java/ru/movie/app/ui/scenes/movielist/FragmentMoviesList.kt
+++ app/src/main/java/ru/movie/app/ui/scenes/movielist/FragmentMoviesList.kt
@@ -1,7 +1,6 @@
 package ru.movie.app.ui.scenes.movielist
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
